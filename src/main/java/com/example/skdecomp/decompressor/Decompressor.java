fe8f92a4c7f727f8bfcb4c402d25c55dfe5a9727
KacperChabros <chabroskacper@gmail.com>
2023-05-04 14:27:52 +0200

    Deleted redundant access modifiers.

--- src/main/java/com/example/skdecomp/decompressor/Decompressor.java
+++ src/main/java/com/example/skdecomp/decompressor/Decompressor.java
@@ -3,5 +3,5 @@ package com.example.skdecomp.decompressor;
 import java.io.IOException;
 
 public interface Decompressor {
-    public void decompress() throws IOException;
+    void decompress() throws IOException;
 }
--- src/main/java/com/example/skdecomp/decompressor/DecompressorFactory.java
+++ src/main/java/com/example/skdecomp/decompressor/DecompressorFactory.java
@@ -4,8 +4,8 @@ import com.example.skdecomp.SkFile;
 import java.io.File;
 
 public interface DecompressorFactory {
-    public DictionaryReader createDictionaryReader(SkFile file);
+    DictionaryReader createDictionaryReader(SkFile file);
 
-    public Decompressor createDecompressor(SkFile file, File outfile);
+    Decompressor createDecompressor(SkFile file, File outfile);
 
 }
